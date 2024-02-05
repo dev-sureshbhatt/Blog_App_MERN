8f38c094c0c3cfc8cc67b664ff8ae791f7c0c23c
Suresh Bhatt <dev.sureshbhatt@gmail.com>
2024-02-06 02:38:30 +0530

    Single Post Page - HTML Data Display from JSON data(from API) done, and also  CSS Styling

--- client/src/pages/SinglePostPage.js
+++ client/src/pages/SinglePostPage.js
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import {format} from 'date-fns'
 import { useParams } from 'react-router-dom'
 
 export default function SinglePostPage() {
+
+    const [postInfo, setPostInfo] = useState('')
 
     const {id} = useParams()
     
@@ -9,16 +12,27 @@ export default function SinglePostPage() {
 
         
         fetch(`http://localhost:4000/post/${id}`).then((response)=>{
-            response.json().then(()=>{
-                console.log("response is", response)
+            response.json().then((postInfo)=>{
+                setPostInfo(postInfo)
             })
         })
     }, [])
 
 
-
+if (!postInfo) return ''
 
   return (
-    <div>SinglePostPage</div>
+
+    <div className='post-page'>
+    <h1>{postInfo.title}</h1>
+    <time>{format(new Date(postInfo.createdAt), 'MMM d, yyyy')}</time>
+    <div className='author'>by @{postInfo.author.displayName}</div>
+    <div className='post-image'>
+        <img src={`http://localhost:4000/${postInfo.cover}`} ></img>
+    </div>
+    
+    <div className='content' dangerouslySetInnerHTML={{__html:postInfo.content}}></div>
+
+    </div>
   )
 }
